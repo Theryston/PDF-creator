6b8cea624436ab665ee08103659ac224262493b4
Theryston <funktodo2@gmail.com>
2020-12-29 08:15:35 -0300

    fix(index.js): get version from github
    using the axios it get the version from package.json in github and compare it to the version the
    user is using
    
    BREAKING CHANGE: the user cannot use the CLI if the version is not updated

--- index.js
+++ index.js
@@ -2,37 +2,48 @@ const command = process.argv
 const aboutProject = require('./package.json')
 const chalk = require('chalk');
 const open = require('open');
-const request = require('request')
+const axios = require('axios')
 
-if (command.indexOf('-v') != -1) {
 
-	console.log(`you're using the version ` + chalk.green(aboutProject.version));
-	
+if (command.indexOf('-v') != -1 || command.indexOf('--version') != -1) {
+	console.log(`you're using the version: ` + chalk.green(aboutProject.version));
+
 } else if (command.indexOf('--help') != -1) {
 
 	console.log(`visit this page:\n\n${chalk.blue('https://github.com/Theryston/PDF-creator/blob/master/README.md')}\n`)
 
 	open('https://github.com/Theryston/PDF-creator/blob/master/README.md');
 
-} else if (request) {
+} else {
 
-	request('https://raw.githubusercontent.com/Theryston/PDF-creator/master/package.json', (error, res) => {
-		if (error) {
-			console.log(chalk.red('ERROR'))
-		} else if (res) {
-			const aboutProjectInGithubMaster = JSON.parse(res.body)
+	void async function() {
+		try {
+			var { data: aboutProjectHosted } = await axios('https://raw.githubusercontent.com/Theryston/PDF-creator/master/package.json')
+		} catch (error) {
+			var aboutProjectHosted = {
+				error: `${chalk.red('ERROR:')} I could not see the available version and there is no way for me to know if your version is stable!`
+			}
+		}
 
-			if (aboutProjectInGithubMaster.version !==  aboutProject.version) {
+
+		if (aboutProjectHosted.error != undefined) {
+			console.log(aboutProjectHosted.error)
+		} else {
+
+			if (aboutProjectHosted.version !== aboutProject.version) {
 
 				console.log(chalk.red('\nERROR:')+ ' the version you are using is not the same version currently available! update the PDF-creator\n\n\n   RUN:\n\n'+chalk.green('   |-------------------------------|\n   |				   |\n   |  	npm i -g PDF-creator   	   |\n   | 			   	   |\n   |-------------------------------|\n'))
-				
+				console.log('\n your version is: '+chalk.red(aboutProject.version) + ' and the available version is: ' + chalk.green(aboutProjectHosted.version)+ '\n')
+
 			} else {
 
+				console.log('alright!')
 				//call robots here
 
 			}
-			
+
 		}
-	})
-	
+
+	}()
+
 }
